3117a4c36bb369870d8488119cc8803cd3d9a777
KolaYAndr <nikolay_andreev02@mail.ru>
2023-08-21 20:13:08 +0300

    Added placeholder for textfields and adaptive icon

--- app/src/main/java/com/example/improgame/utils/Constants.kt
+++ app/src/main/java/com/example/improgame/utils/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.improgame.utils
 
+import androidx.compose.ui.graphics.Color
+
 class Constants {
     companion object {
         const val fontSizeMedium = 26
@@ -7,11 +9,14 @@ class Constants {
         const val fontSizeSmall = 24
 
         const val iconSize = 32
-        const val minHeight = 150
-        const val maxHeight = 250
+        const val minSize = 150
+        const val maxSize = 250
         const val paddingValue = 10
 
         const val borderStrokeWidth = 3
         const val roundedCorner = 2
+
+        val colorBlue = Color(28, 100, 242)
+        val colorRed = Color(240, 82, 82)
     }
 }
